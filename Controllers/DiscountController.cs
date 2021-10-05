15b08b25bce99f4fcfc557542a5b2befabd132e0
Thato Ramalepa <tramalepa@my.wctc.edu>
2021-10-05 00:22:55 -0500

    first commit

---
+++ Controllers/DiscountController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+using System.Linq;
+using System;
+
+namespace Northwind.Controllers
+{
+    public class DiscountController : Controller
+    {
+        // private NorthwindContext _NorthwindContext;
+
+        // public ProductController(NorthwindContext db) => _NorthwindContext = db;
+    }
+}
